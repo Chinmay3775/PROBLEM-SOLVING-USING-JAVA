4b5ef51439951c5a18da009ce34499500535a62f
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-11-01 11:13:21 +0530

    Update string_reverse.java

--- string_reverse.java
+++ string_reverse.java
@@ -1,16 +1,11 @@
 import java.io.*;
 import java.util.*;
 public class string_reverse {
-    
-
-
-
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String rev="";
-        /* Enter your code here. Print output to STDOUT. */
         for(int i=A.length()-1;i>=0;i--){
              rev+=A.charAt(i);
         }
