11289006f2f8f4937de755e7b0cb72cd11b895e6
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-11-01 11:10:01 +0530

    1957. Delete Characters to Make Fancy String.java

---
+++ leetcode/Code Testcase Test Result Test Result 1957. Delete Characters to Make Fancy String.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder result = new StringBuilder(s.length());
+        int count = 1;
+        result.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count < 3) {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
