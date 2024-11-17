70e176a08558a963c4693633233d231b6882f664
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-11-17 10:32:07 +0530

    Create 492. Construct the Rectangle.java

---
+++ leetcode/492. Construct the Rectangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        int w=(int)Math.sqrt(area);
+        while(area%w!=0){
+            w-=1;
+        }
+        int l=area/w;
+        return new int[]{l,w};
+    }
+}
