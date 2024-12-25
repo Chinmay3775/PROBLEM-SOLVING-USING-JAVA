2f6682e239c4b8b04439b322651db7f1763a7fa1
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-12-25 12:01:37 +0530

    Update Number_game.java

--- Number_game.java
+++ Number_game.java
@@ -4,26 +4,61 @@ import java.util.Random;
 class handle_number{
     int lb=1;
     int up=100;
-    int max_atp=5;
+    int max_atp=10;
     int score=0;
     boolean playagain =true;
 
-    Random r = new Random();
+    Random r = new Random(); 
     int rn=r.nextInt(up-lb+1)+lb;
-    void show(){
-        System.out.println(rn);    
+   
+    void reset(){
+        rn=r.nextInt(up-lb+1)+lb;
+        max_atp=10;
+
+    }
+    boolean guess(int num){
+        if(num==rn) {
+            System.out.println("You Guessed correct Number!!");
+            score++;
+            return true;
+        }
+        else if(num>rn) System.out.println("Too high! Try Again");
+        else System.out.println("Too Low! TryAgain");
+        max_atp--;
+        if(max_atp==0){
+            System.out.println("Out of attempts! The correct Number was: "+rn);
+            return true;
+        }
+        return false;
     }
 }
 public class Number_game {
     public static void main(String[] args) {
-        System.out.println("Guess the number Between 0-100: ");
         Scanner sc=new Scanner(System.in);
-        int number=sc.nextInt();
-        if(number<=100){
         handle_number obj= new handle_number();
-        obj.show();
+
+        while(obj.playagain){
+            System.out.println("Guess the number Between 0-100: \n You have "+obj.max_atp +" to Guess the correct Number ");
+            
+            while(obj.max_atp>0){
+                System.out.println("Enter the Guess Number");
+                int number=sc.nextInt();
+                
+                if(obj.guess(number)){
+                    break;
+                }
+                
+                System.out.println("Attempts Remaining:"+obj.max_atp);
+            }System.out.println("1.Play Again");
+            System.out.println("2.Exit");
+            int play=sc.nextInt();
+
+            switch(play){
+                case 1 -> obj.reset();
+                case 2->obj.playagain=false;
+
+            }
         }
-        else System.err.println("Enter the Number within the Range!!");
-        
+        sc.close();    
     }
 }
