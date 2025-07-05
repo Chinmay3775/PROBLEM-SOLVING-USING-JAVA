5491c0d208d8b2987d0b30c6d33f700867b6d229
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-07-05 11:30:34 +0530

    Update 217. Contains Duplicate.java

--- leetcode/217. Contains Duplicate.java
+++ leetcode/217. Contains Duplicate.java
@@ -1,9 +1,41 @@
+//Solution 1
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i =0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1])
             return true;
+        }
+        return false;
+    }
+}
+//Solution 2
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for(int i:nums){
+            if(map.contains(i)){
+                return true;
+            }
+            map.add(i);
+        }
+        return false;
+    }
+}
+
+//Solution 3
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    Map<Integer,Integer> map = new HashMap<>();
+
+    for(int i : nums){
+            
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i : nums){
+            if(1<map.get(i)){
+                return true;
+            }
         }
         return false;
     }
