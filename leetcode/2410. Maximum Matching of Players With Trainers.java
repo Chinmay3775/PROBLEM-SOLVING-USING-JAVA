32f38fb3a8f2a74b66311a738396c371fe8fd8bf
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-07-13 23:54:15 +0530

    Update 2410. Maximum Matching of Players With Trainers.java

--- leetcode/2410. Maximum Matching of Players With Trainers.java
+++ leetcode/2410. Maximum Matching of Players With Trainers.java
@@ -15,3 +15,22 @@ class Solution {
         return content;
     }
 }
+
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+     Arrays.sort(players);
+     Arrays.sort(trainers);
+     int i=0;
+     int j=0;
+     int count=0;
+     while(i<players.length && j<trainers.length ){
+            if(players[i]<=trainers[j]){
+                count++;
+                trainers[j]=0;
+                i++;
+                j++;
+            }else j++;
+        }
+        return count;    
+    }
+}
