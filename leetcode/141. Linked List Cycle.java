b4c019c40a6da9fa718049747229c56c64f92a15
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-07-25 19:16:35 +0530

    Create 141. Linked List Cycle.java

---
+++ leetcode/141. Linked List Cycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        //Slow fast approach
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
+//When we use Slow Fast approach at a certain point slow and fast will be at same point if it is circularLinkedList if it doesn't pass this then return False 
