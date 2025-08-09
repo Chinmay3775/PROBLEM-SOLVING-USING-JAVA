886f47cfdf6c2db88fca414c02411dc9e7595d4b
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-08-09 15:45:35 +0530

    Create 1672. Richest Customer Wealth.java

---
+++ leetcode/1672. Richest Customer Wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=Integer.MIN_VALUE;
+        for(int[] i: accounts){
+            int temp=0;
+            for(int j: i ){
+                temp+= j;
+            }
+            if(temp>max) max=temp;
+        }
+        return max;
+    }
+}
