b7fb5388c06c85608ba6b61b49149be0579b3968
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-08-11 21:13:18 +0530

    Update 509. Fibonacci Number.java

--- leetcode/509. Fibonacci Number.java
+++ leetcode/509. Fibonacci Number.java
@@ -1,14 +1,32 @@
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) {
+//             return n;
+//         }
+//         int[] fibCache = new int[n + 1];
+//         fibCache[0] = 0;
+//         fibCache[1] = 1;
+//         for (int i = 2; i <= n; i++) {
+//             fibCache[i] = fibCache[i - 1] + fibCache[i - 2];
+//         }
+//         return fibCache[n];
+//     }
+// }
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
-            return n;
+        if(n<=1) return n;
+        return F(n);
+    }
+    static int F(int n){
+        int a=0;
+        int b=1;
+        int count =2;
+        while(count<=n){
+            int temp=b;
+            b=b+a;
+            a=temp;
+            count ++;
         }
-        int[] fibCache = new int[n + 1];
-        fibCache[0] = 0;
-        fibCache[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            fibCache[i] = fibCache[i - 1] + fibCache[i - 2];
-        }
-        return fibCache[n];
+        return b;
     }
 }
