beb0fe8f180dee87543666dc2551c43459ac6d9c
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-09-18 18:49:14 +0530

    Create 118. Pascal's Triangle.java

---
+++ leetcode/118. Pascal's Triangle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(numRows==0) return result;
+
+        List<Integer> fstrow= new ArrayList<>();
+        fstrow.add(1);
+        result.add(fstrow);
+
+        if(numRows==1) return result;
+
+        for(int i=1;i<numRows;i++){
+            List<Integer> prevRow=result.get(i-1);
+            ArrayList<Integer> row= new ArrayList<>();
+            row.add(1);
+            for(int j=0;j<i-1;j++){
+                row.add(prevRow.get(j)+prevRow.get(j+1));
+            }
+            row.add(1);
+            result.add(row);
+        }
+        return result;
+    }
+}
